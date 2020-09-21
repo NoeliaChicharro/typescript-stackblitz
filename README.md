884f146216eec38710bfd770d0059e1d17b9f176
NoeliaChicharro <nch@marisma.com>
2020-09-21 19:00:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-h1wrm4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-h1wrm4)
---
+++ index.ts
@@ -0,0 +1,7 @@
+let selection: string | null;
+
+selection = "item 1" as any;
+
+if(selection !== null){
+  console.log(selection.charAt(5));
+}
